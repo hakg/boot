a53bbcdae9139953ff564f7a6839c2a814025b9f
HakG <tisccool@gmail.com>
2022-11-22 00:59:48 +0900

    .idea 폴더 커밋제외

---
+++ src/main/java/com/hakg/Application.java
@@ -0,0 +1,11 @@
+package com.hakg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
