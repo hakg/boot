9f2b908f9627e2bee6e9cb74f2096dd19b71d628
HakG <tisccool@gmail.com>
2022-12-09 01:11:34 +0900

    스프링부트 4장 테스트 코드 실습

--- src/main/java/com/hakg/service/PostsService.java
+++ src/main/java/com/hakg/service/PostsService.java
@@ -2,12 +2,16 @@ package com.hakg.service;
 
 import com.hakg.domain.posts.Posts;
 import com.hakg.domain.posts.PostsRepository;
+import com.hakg.web.dto.PostsListResponseDto;
 import com.hakg.web.dto.PostsResponseDto;
 import com.hakg.web.dto.PostsSaveRequestDto;
 import com.hakg.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RequiredArgsConstructor
@@ -35,5 +39,12 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id =" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/com/hakg/web/PostsApiController.java
+++ src/main/java/com/hakg/web/PostsApiController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 public class PostsApiController {
     private final PostsService postsService;
 
-    @PostMapping("/api/v1/posts/{id}")
+    @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
     }
