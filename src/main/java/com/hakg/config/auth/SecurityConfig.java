ed58af348d5539a578e72ea1698934cc7b61abc6
HakG <tisccool@gmail.com>
2022-12-15 00:14:32 +0900

    스프링부트 5장 구글로그인 테스트 코드 실습

---
+++ src/main/java/com/hakg/config/auth/SecurityConfig.java
@@ -0,0 +1,11 @@
+package com.hakg.config.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//    private final CustomOA
+}
