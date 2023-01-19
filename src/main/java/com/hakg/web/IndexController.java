2b4b5219bca5daa0e791d95c78b0c5f0fb7c83fe
HakG <tisccool@gmail.com>
2023-01-20 00:36:54 +0900

    스프링부트 5장 네이버로그인 테스트 코드 실습

--- src/main/java/com/hakg/web/IndexController.java
+++ src/main/java/com/hakg/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.hakg.web;
 
+import com.hakg.config.auth.LoginUser;
 import com.hakg.config.auth.dto.SessionUser;
 import com.hakg.service.PostsService;
 import com.hakg.web.dto.PostsResponseDto;
@@ -17,9 +18,8 @@ public class IndexController {
     private final PostsService postsService;
     private final HttpSession httpSession;
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if (user != null) {
             model.addAttribute("userName", user.getName());
